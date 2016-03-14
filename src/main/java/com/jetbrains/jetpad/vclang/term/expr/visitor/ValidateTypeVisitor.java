76926fffb0984f637b912219f69eb152b1cb3a8c
Andrey Komarov <taukus@gmail.com>
2016-03-14 16:48:10 +0300

    normalize type, add branch elim tree checking

--- src/main/java/com/jetbrains/jetpad/vclang/term/expr/visitor/ValidateTypeVisitor.java
+++ src/main/java/com/jetbrains/jetpad/vclang/term/expr/visitor/ValidateTypeVisitor.java
@@ -2,12 +2,11 @@ package com.jetbrains.jetpad.vclang.term.expr.visitor;
 
 import com.jetbrains.jetpad.vclang.term.context.param.DependentLink;
 import com.jetbrains.jetpad.vclang.term.expr.*;
-import com.jetbrains.jetpad.vclang.term.pattern.elimtree.BranchElimTreeNode;
-import com.jetbrains.jetpad.vclang.term.pattern.elimtree.EmptyElimTreeNode;
-import com.jetbrains.jetpad.vclang.term.pattern.elimtree.LeafElimTreeNode;
+import com.jetbrains.jetpad.vclang.term.pattern.elimtree.*;
 import com.jetbrains.jetpad.vclang.term.pattern.elimtree.visitor.ElimTreeNodeVisitor;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -46,15 +45,6 @@ public class ValidateTypeVisitor extends BaseExpressionVisitor<Expression, Void>
     this.myErrorReporter = errorReporter;
   }
 
-  private void visitDependentLink(DependentLink link) {
-    if (!link.isExplicit()) {
-      myErrorReporter.addError(link.getType(), "Explicit argument expected");
-    }
-    if (link.hasNext()) {
-      visitDependentLink(link.getNext());
-    }
-  }
-
   @Override
   public Void visitDefCall(DefCallExpression expr, Expression expectedType) {
     expr.getType().accept(this, expectedType);
@@ -65,7 +55,7 @@ public class ValidateTypeVisitor extends BaseExpressionVisitor<Expression, Void>
   public Void visitApp(AppExpression expr, Expression expectedType) {
     ArrayList<ArgumentExpression> args = new ArrayList<>();
     Expression fun = expr.getFunctionArgs(args);
-    Expression funType = fun.getType();
+    Expression funType = fun.getType().normalize(NormalizeVisitor.Mode.NF);
     if (!(funType instanceof PiExpression)) {
       myErrorReporter.addError(expr, "Function " + fun + " doesn't have Pi-type");
     } else {
@@ -78,13 +68,16 @@ public class ValidateTypeVisitor extends BaseExpressionVisitor<Expression, Void>
       }
       for (int i = 0; i < args.size(); i++) {
         DependentLink param = links.get(i);
-        Expression arg = args.get(i).getExpression();
-        Expression argType = arg.getType().normalize(NormalizeVisitor.Mode.NF);
+        ArgumentExpression arg = args.get(i);
+        Expression argType = arg.getExpression().getType().normalize(NormalizeVisitor.Mode.NF);
         Expression expectedArgType = param.getType().subst(subst).normalize(NormalizeVisitor.Mode.NF);
-        if (!argType.equals(expectedArgType)) {
-          myErrorReporter.addError(arg, "Expected type: " + expectedArgType + ", actual: " + argType);
+        if (param.isExplicit() != arg.isExplicit()) {
+          myErrorReporter.addError(expr, i + "th argument is expected to be" + (param.isExplicit() ? "explicit" : "implicit"));
         }
-        subst.add(param, arg);
+        if (!argType.equals(expectedArgType)) {
+          myErrorReporter.addError(arg.getExpression(), "Expected type: " + expectedArgType + ", actual: " + argType);
+        }
+        subst.add(param, arg.getExpression());
       }
     }
 
@@ -93,22 +86,20 @@ public class ValidateTypeVisitor extends BaseExpressionVisitor<Expression, Void>
 
   @Override
   public Void visitReference(ReferenceExpression expr, Expression expectedType) {
-    expr.getType().accept(this, expectedType);
-    expr.getBinding().getType().accept(this, expectedType);
     return null;
   }
 
   @Override
   public Void visitLam(LamExpression expr, Expression expectedType) {
-    visitDependentLink(expr.getParameters());
-    expr.getType().accept(this, expectedType);
+    for (DependentLink params = expr.getParameters(); params.hasNext(); params = params.getNext()) {
+      params.getType().accept(this, null);
+    }
     expr.getBody().accept(this, expectedType);
     return null;
   }
 
   @Override
   public Void visitPi(PiExpression expr, Expression expectedType) {
-    visitDependentLink(expr.getParameters());
     expr.getType().accept(this, expectedType);
     expr.getCodomain().accept(this, expectedType);
     return null;
@@ -116,7 +107,6 @@ public class ValidateTypeVisitor extends BaseExpressionVisitor<Expression, Void>
 
   @Override
   public Void visitSigma(SigmaExpression expr, Expression expectedType) {
-    visitDependentLink(expr.getParameters());
     expr.getType().accept(this, expectedType);
     return null;
   }
@@ -137,7 +127,6 @@ public class ValidateTypeVisitor extends BaseExpressionVisitor<Expression, Void>
     SigmaExpression type = expr.getType();
     type = (SigmaExpression) type.normalize(NormalizeVisitor.Mode.NF);
     DependentLink link = type.getParameters();
-    visitDependentLink(link);
     type.accept(this, expectedType);
     Substitution subst = new Substitution();
     for (Expression field : expr.getFields()) {
@@ -181,6 +170,7 @@ public class ValidateTypeVisitor extends BaseExpressionVisitor<Expression, Void>
 
   @Override
   public Void visitLet(LetExpression letExpression, Expression expectedType) {
+    Substitution subst = new Substitution();
     for (LetClause clause : letExpression.getClauses()) {
       clause.getElimTree().accept(this, clause.getType());
     }
@@ -189,6 +179,14 @@ public class ValidateTypeVisitor extends BaseExpressionVisitor<Expression, Void>
 
   @Override
   public Void visitBranch(BranchElimTreeNode branchNode, Expression expectedType) {
+    Collection<ConstructorClause> clauses = branchNode.getConstructorClauses();
+    for (ConstructorClause clause : clauses) {
+      clause.getChild().accept(this, expectedType);
+    }
+    OtherwiseClause otherwiseClause = branchNode.getOtherwiseClause();
+    if (otherwiseClause != null) {
+      otherwiseClause.getChild().accept(this, expectedType);
+    }
     return null;
   }
 
