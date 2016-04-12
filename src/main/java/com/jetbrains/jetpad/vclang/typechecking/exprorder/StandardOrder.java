5ceb07de745fca114f5da289a0f986194e2eb8dd
part-xx <fedor.part@gmail.com>
2016-04-12 11:45:37 +0300

    Add comparison and max for universes to StandardOrder

--- src/main/java/com/jetbrains/jetpad/vclang/typechecking/exprorder/StandardOrder.java
+++ src/main/java/com/jetbrains/jetpad/vclang/typechecking/exprorder/StandardOrder.java
@@ -1,9 +1,8 @@
 package com.jetbrains.jetpad.vclang.typechecking.exprorder;
 
 import com.jetbrains.jetpad.vclang.term.Preprelude;
-import com.jetbrains.jetpad.vclang.term.expr.ClassCallExpression;
-import com.jetbrains.jetpad.vclang.term.expr.DataCallExpression;
-import com.jetbrains.jetpad.vclang.term.expr.Expression;
+import com.jetbrains.jetpad.vclang.term.definition.TypeUniverse;
+import com.jetbrains.jetpad.vclang.term.expr.*;
 import com.jetbrains.jetpad.vclang.term.expr.visitor.CompareVisitor;
 import com.jetbrains.jetpad.vclang.term.expr.visitor.NormalizeVisitor;
 import com.jetbrains.jetpad.vclang.typechecking.implicitargs.equations.Equations;
@@ -12,8 +11,11 @@ public class StandardOrder implements ExpressionOrder {
 
   @Override
   public boolean compare(Expression expr1, Expression expr2, CompareVisitor visitor, Equations.CMP expectedCMP) {
-    return CNatOrder.compareCNat(expr1, expr2, visitor, expectedCMP) || LvlOrder.compareLvl(expr1, expr2, visitor, expectedCMP) ||
-            LevelOrder.compareLevel(expr1, expr2, visitor, expectedCMP) || NatOrder.compareNat(expr1, expr2, visitor, expectedCMP);
+    if(CNatOrder.compareCNat(expr1, expr2, visitor, expectedCMP) || LvlOrder.compareLvl(expr1, expr2, visitor, expectedCMP) ||
+            LevelOrder.compareLevel(expr1, expr2, visitor, expectedCMP) || NatOrder.compareNat(expr1, expr2, visitor, expectedCMP)) return true;
+    UniverseExpression uni1 = expr1.toUniverse();
+    UniverseExpression uni2 = expr2.toUniverse();
+    return uni1 != null && uni2 != null && uni1.getUniverse().compare(uni2.getUniverse(), visitor, expectedCMP);
   }
 
   @Override
@@ -49,6 +51,20 @@ public class StandardOrder implements ExpressionOrder {
       }
     }
 
-    return null;
+    UniverseExpression uni1 = type1.toUniverse();
+    UniverseExpression uni2 = type2.toUniverse();
+
+    if (uni1 == null || uni2 == null || !(uni1.getUniverse() instanceof TypeUniverse) || !(uni2.getUniverse() instanceof TypeUniverse)) {
+      return null;
+    }
+
+    TypeUniverse.TypeLevel level1 = ((TypeUniverse) uni1.getUniverse()).getLevel();
+    TypeUniverse.TypeLevel level2 = ((TypeUniverse) uni2.getUniverse()).getLevel();
+
+    if (level1 == null || level2 == null) {
+      return null;
+    }
+
+    return ExpressionFactory.Universe(level1.max(level2).getValue());
   }
 }
