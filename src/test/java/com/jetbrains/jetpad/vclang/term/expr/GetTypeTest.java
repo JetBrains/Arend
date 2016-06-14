30e7ef06a7f019932fdb9a505273e812ee81be43
Kirill Elagin <kirelagin@gmail.com>
2016-06-14 20:21:38 +0300

    GetTypeTest: fix tests

--- src/test/java/com/jetbrains/jetpad/vclang/term/expr/GetTypeTest.java
+++ src/test/java/com/jetbrains/jetpad/vclang/term/expr/GetTypeTest.java
@@ -1,11 +1,11 @@
 package com.jetbrains.jetpad.vclang.term.expr;
 
-import com.jetbrains.jetpad.vclang.naming.NamespaceMember;
 import com.jetbrains.jetpad.vclang.term.Prelude;
 import com.jetbrains.jetpad.vclang.term.context.param.DependentLink;
 import com.jetbrains.jetpad.vclang.term.definition.*;
 import com.jetbrains.jetpad.vclang.term.expr.visitor.NormalizeVisitor;
 import com.jetbrains.jetpad.vclang.term.pattern.elimtree.LeafElimTreeNode;
+import com.jetbrains.jetpad.vclang.typechecking.TypeCheckingTestCase;
 import org.junit.Test;
 
 import java.util.EnumSet;
@@ -16,63 +16,64 @@ import static com.jetbrains.jetpad.vclang.typechecking.TypeCheckingTestCase.type
 import static org.junit.Assert.assertEquals;
 
 public class GetTypeTest {
+  private static void testType(Expression expected, TypeCheckingTestCase.TypeCheckClassResult result) {
+    assertEquals(expected, ((FunctionDefinition) result.getDefinition("test")).getResultType());
+    assertEquals(expected, ((LeafElimTreeNode) ((FunctionDefinition) result.getDefinition("test")).getElimTree()).getExpression().getType());
+  }
+
   @Test
   public void constructorTest() {
-    NamespaceMember member = typeCheckClass("\\static \\data List (A : \\Type0) | nil | cons A (List A) \\static \\function test => cons 0 nil");
-    assertEquals(Apps(member.namespace.getDefinition("List").getDefCall(), Nat()), member.namespace.getDefinition("test").getType());
-    assertEquals(Apps(member.namespace.getDefinition("List").getDefCall(), Nat()), ((LeafElimTreeNode) ((FunctionDefinition) member.namespace.getDefinition("test")).getElimTree()).getExpression().getType());
+    TypeCheckingTestCase.TypeCheckClassResult result = typeCheckClass("\\static \\data List (A : \\Type0) | nil | cons A (List A) \\static \\function test => cons 0 nil");
+    testType(Apps(result.getDefinition("List").getDefCall(), Nat()), result);
   }
 
   @Test
   public void nilConstructorTest() {
-    NamespaceMember member = typeCheckClass("\\static \\data List (A : \\Type0) | nil | cons A (List A) \\static \\function test => (List Nat).nil");
-    assertEquals(Apps(member.namespace.getDefinition("List").getDefCall(), Nat()), member.namespace.getDefinition("test").getType());
-    assertEquals(Apps(member.namespace.getDefinition("List").getDefCall(), Nat()), ((LeafElimTreeNode) ((FunctionDefinition) member.namespace.getDefinition("test")).getElimTree()).getExpression().getType());
+    TypeCheckingTestCase.TypeCheckClassResult result = typeCheckClass("\\static \\data List (A : \\Type0) | nil | cons A (List A) \\static \\function test => (List Nat).nil");
+    testType(Apps(result.getDefinition("List").getDefCall(), Nat()), result);
   }
 
   @Test
   public void classExtTest() {
-    NamespaceMember member = typeCheckClass("\\static \\class Test { \\abstract A : \\Type0 \\abstract a : A } \\static \\function test => Test { A => Nat }");
-    assertEquals(Universe(1), member.namespace.getDefinition("Test").getType());
-    assertEquals(Universe(0), member.namespace.getDefinition("test").getType());
-    assertEquals(Universe(TypeUniverse.SetOfLevel(0)), ((LeafElimTreeNode) ((FunctionDefinition) member.namespace.getDefinition("test")).getElimTree()).getExpression().getType());
+    TypeCheckingTestCase.TypeCheckClassResult result = typeCheckClass("\\static \\class Test { \\abstract A : \\Type0 \\abstract a : A } \\static \\function test => Test { A => Nat }");
+    assertEquals(Universe(1), result.getDefinition("Test").getType());
+    assertEquals(Universe(0), result.getDefinition("test").getType());
+    testType(Universe(TypeUniverse.SetOfLevel(0)), result);
   }
 
   @Test
   public void lambdaTest() {
-    Definition def = typeCheckDef("\\function test => \\lam (f : Nat -> Nat) => f 0");
-    assertEquals(Pi(Pi(Nat(), Nat()), Nat()), def.getType());
-    assertEquals(Pi(Pi(Nat(), Nat()), Nat()), ((LeafElimTreeNode) ((FunctionDefinition) def).getElimTree()).getExpression().getType());
+    TypeCheckingTestCase.TypeCheckClassResult result = typeCheckClass("\\static \\function test => \\lam (f : Nat -> Nat) => f 0");
+    testType(Pi(Pi(Nat(), Nat()), Nat()), result);
   }
 
   @Test
   public void lambdaTest2() {
-    Definition def = typeCheckDef("\\function test => \\lam (A : \\Type0) (x : A) => x");
+    TypeCheckingTestCase.TypeCheckClassResult result = typeCheckClass("\\function test => \\lam (A : \\Type0) (x : A) => x");
     DependentLink A = param("A", Universe(0));
     Expression expectedType = Pi(params(A, param("x", Reference(A))), Reference(A));
-    assertEquals(expectedType, def.getType());
-    assertEquals(expectedType, ((LeafElimTreeNode) ((FunctionDefinition) def).getElimTree()).getExpression().getType());
+    testType(expectedType, result);
   }
 
   @Test
   public void fieldAccTest() {
-    NamespaceMember member = typeCheckClass("\\static \\class C { \\abstract x : Nat \\function f (p : 0 = x) => p } \\static \\function test (p : Nat -> C) => (p 0).f");
-    DependentLink p = param("p", Pi(Nat(), member.namespace.getDefinition("C").getDefCall()));
+    TypeCheckingTestCase.TypeCheckClassResult result = typeCheckClass("\\static \\class C { \\abstract x : Nat \\function f (p : 0 = x) => p } \\static \\function test (p : Nat -> C) => (p 0).f");
+    DependentLink p = param("p", Pi(Nat(), result.getDefinition("C").getDefCall()));
     Expression type = FunCall(Prelude.PATH_INFIX)
       .addArgument(ZeroLvl(), EnumSet.noneOf(AppExpression.Flag.class))
             .addArgument(Fin(Suc(Zero())), EnumSet.noneOf(AppExpression.Flag.class))
       .addArgument(Nat(), EnumSet.noneOf(AppExpression.Flag.class))
       .addArgument(Zero(), AppExpression.DEFAULT)
-      .addArgument(Apps(member.namespace.getMember("C").namespace.getDefinition("x").getDefCall(), Apps(Reference(p), Zero())), AppExpression.DEFAULT);
-    assertEquals(Pi(p, Pi(type, type)).normalize(NormalizeVisitor.Mode.NF), member.namespace.getDefinition("test").getType().normalize(NormalizeVisitor.Mode.NF));
+      .addArgument(Apps(result.getDefinition("C.x").getDefCall(), Apps(Reference(p), Zero())), AppExpression.DEFAULT);
+    assertEquals(Pi(p, Pi(type, type)).normalize(NormalizeVisitor.Mode.NF), result.getDefinition("test").getType().normalize(NormalizeVisitor.Mode.NF));
   }
 
   @Test
   public void tupleTest() {
-    Definition def = typeCheckDef("\\function test : \\Sigma (x y : Nat) (x = y) => (0, 0, path (\\lam _ => 0))");
+    TypeCheckingTestCase.TypeCheckClassResult result = typeCheckClass("\\function test : \\Sigma (x y : Nat) (x = y) => (0, 0, path (\\lam _ => 0))");
     DependentLink xy = param(true, vars("x", "y"), Nat());
-    assertEquals(Sigma(params(xy, param(Apps(FunCall(Prelude.PATH_INFIX).addArgument(ZeroLvl(), EnumSet.noneOf(AppExpression.Flag.class)).addArgument(Fin(Suc(Zero())), EnumSet.noneOf(AppExpression.Flag.class))
-            .addArgument(Nat(), EnumSet.noneOf(AppExpression.Flag.class)), Reference(xy), Reference(xy.getNext()))))), ((LeafElimTreeNode)((FunctionDefinition) def).getElimTree()).getExpression().getType());
+    testType(Sigma(params(xy, param(Apps(FunCall(Prelude.PATH_INFIX).addArgument(ZeroLvl(), EnumSet.noneOf(AppExpression.Flag.class)).addArgument(Fin(Suc(Zero())), EnumSet.noneOf(AppExpression.Flag.class))
+            .addArgument(Nat(), EnumSet.noneOf(AppExpression.Flag.class)), Reference(xy), Reference(xy.getNext()))))), result);
   }
 
   @Test
@@ -86,9 +87,9 @@ public class GetTypeTest {
 
   @Test
   public void patternConstructor1() {
-    NamespaceMember member = typeCheckClass(
+    TypeCheckingTestCase.TypeCheckClassResult result = typeCheckClass(
         "\\static \\data C (n : Nat) | C (zero) => c1 | C (suc n) => c2 Nat");
-    DataDefinition data = (DataDefinition) member.namespace.getMember("C").definition;
+    DataDefinition data = (DataDefinition) result.getDefinition("C");
     assertEquals(Apps(data.getDefCall(), Zero()), data.getConstructor("c1").getType());
     DependentLink params = data.getConstructor("c2").getDataTypeParameters();
     assertEquals(
@@ -99,11 +100,11 @@ public class GetTypeTest {
 
   @Test
   public void patternConstructor2() {
-    NamespaceMember member = typeCheckClass(
+    TypeCheckingTestCase.TypeCheckClassResult result = typeCheckClass(
         "\\static \\data Vec \\Type0 Nat | Vec A zero => Nil | Vec A (suc n) => Cons A (Vec A n)" +
         "\\static \\data D (n : Nat) (Vec Nat n) | D zero _ => dzero | D (suc n) _ => done");
-    DataDefinition vec = (DataDefinition) member.namespace.getMember("Vec").definition;
-    DataDefinition d = (DataDefinition) member.namespace.getMember("D").definition;
+    DataDefinition vec = (DataDefinition) result.getDefinition("Vec");
+    DataDefinition d = (DataDefinition) result.getDefinition("D");
     assertEquals(
         Pi(d.getConstructor("dzero").getDataTypeParameters(), Apps(DataCall(d), Zero(), Reference(d.getConstructor("dzero").getDataTypeParameters()))),
         d.getConstructor("dzero").getType()
@@ -122,11 +123,11 @@ public class GetTypeTest {
 
   @Test
   public void patternConstructor3() {
-    NamespaceMember member = typeCheckClass(
+    TypeCheckingTestCase.TypeCheckClassResult result = typeCheckClass(
         "\\static \\data D | d \\Type0\n" +
         "\\static \\data C D | C (d A) => c A");
-    DataDefinition d = (DataDefinition) member.namespace.getMember("D").definition;
-    DataDefinition c = (DataDefinition) member.namespace.getMember("C").definition;
+    DataDefinition d = (DataDefinition) result.getDefinition("D");
+    DataDefinition c = (DataDefinition) result.getDefinition("C");
     DependentLink A = c.getConstructor("c").getDataTypeParameters();
     assertEquals(
         Pi(c.getConstructor("c").getDataTypeParameters(), Pi(Reference(A), Apps(DataCall(c), Apps(ConCall(d.getConstructor("d")), Reference(A))))),
@@ -136,10 +137,10 @@ public class GetTypeTest {
 
   @Test
   public void patternConstructorDep() {
-    NamespaceMember member = typeCheckClass(
+    TypeCheckingTestCase.TypeCheckClassResult result = typeCheckClass(
         "\\static \\data Box (n : Nat) | box\n" +
         "\\static \\data D (n : Nat) (Box n) | D (zero) _ => d");
-    DataDefinition d = (DataDefinition) member.namespace.getMember("D").definition;
+    DataDefinition d = (DataDefinition) result.getDefinition("D");
     assertEquals(
         Pi(d.getConstructor("d").getDataTypeParameters(), Apps(DataCall(d), Zero(), Reference(d.getConstructor("d").getDataTypeParameters()))),
         d.getConstructor("d").getType()
