cc9fc81daad92aadd78be0fb8c01efc2d45b0da5
Kirill Elagin <kirelagin@gmail.com>
2016-11-21 19:38:06 +0300

    Cleanup main, improve output

--- src/main/java/com/jetbrains/jetpad/vclang/ConsoleMain.java
+++ src/main/java/com/jetbrains/jetpad/vclang/ConsoleMain.java
@@ -1,12 +1,12 @@
 package com.jetbrains.jetpad.vclang;
 
-import com.jetbrains.jetpad.vclang.error.DummyErrorReporter;
+import com.jetbrains.jetpad.vclang.error.*;
 import com.jetbrains.jetpad.vclang.error.Error;
-import com.jetbrains.jetpad.vclang.error.GeneralError;
-import com.jetbrains.jetpad.vclang.error.ListErrorReporter;
 import com.jetbrains.jetpad.vclang.module.BaseModuleLoader;
-import com.jetbrains.jetpad.vclang.module.ModuleLoader;
-import com.jetbrains.jetpad.vclang.module.source.*;
+import com.jetbrains.jetpad.vclang.module.source.CompositeSourceSupplier;
+import com.jetbrains.jetpad.vclang.module.source.CompositeStorage;
+import com.jetbrains.jetpad.vclang.module.source.SourceModuleLoader;
+import com.jetbrains.jetpad.vclang.module.source.SourceSupplier;
 import com.jetbrains.jetpad.vclang.module.source.file.FileStorage;
 import com.jetbrains.jetpad.vclang.module.utils.LoadModulesRecursively;
 import com.jetbrains.jetpad.vclang.naming.namespace.*;
@@ -15,6 +15,7 @@ import com.jetbrains.jetpad.vclang.naming.oneshot.OneshotSourceInfoCollector;
 import com.jetbrains.jetpad.vclang.term.Abstract;
 import com.jetbrains.jetpad.vclang.term.ConcreteResolveListener;
 import com.jetbrains.jetpad.vclang.term.Prelude;
+import com.jetbrains.jetpad.vclang.term.SourceInfoProvider;
 import com.jetbrains.jetpad.vclang.typechecking.SimpleTypecheckerState;
 import com.jetbrains.jetpad.vclang.typechecking.TypecheckedReporter;
 import com.jetbrains.jetpad.vclang.typechecking.TypecheckerState;
@@ -32,7 +33,6 @@ import java.util.*;
 
 public class ConsoleMain {
   private static Options cmdOptions = new Options();
-
   static {
     cmdOptions.addOption("h", "help", false, "print this message");
     cmdOptions.addOption(Option.builder("s").longOpt("source").hasArg().argName("dir").desc("source directory").build());
@@ -41,101 +41,78 @@ public class ConsoleMain {
     cmdOptions.addOption(Option.builder().longOpt("recompile").desc("recompile files").build());
   }
 
-  public static void main(String[] args) {
-    final CommandLine cmdLine;
-    try {
-      cmdLine = new DefaultParser().parse(cmdOptions, args);
-    } catch (ParseException e) {
-      System.err.println(e.getMessage());
-      return;
-    }
+  private final List<String> argFiles;
 
-    if (cmdLine.hasOption("h")) {
-      new HelpFormatter().printHelp("vclang [FILES]", cmdOptions);
-    }
+  private final ListErrorReporter errorReporter = new ListErrorReporter();
+  private final ErrorFormatter errf;
 
-    final String sourceDirStr = cmdLine.getOptionValue("s");
-    final File sourceDir = new File(sourceDirStr == null ? System.getProperty("user.dir") : sourceDirStr);
+  // Storage
+  private final String sourceDirStr;
+  private final File sourceDir;
+  private final CompositeStorage<Prelude.SourceId, FileStorage.SourceId> storage;
+
+  // Modules
+  private final SourceModuleLoader<CompositeSourceSupplier<Prelude.SourceId, FileStorage.SourceId>.SourceId> moduleLoader;
+  private final Map<CompositeSourceSupplier<Prelude.SourceId, FileStorage.SourceId>.SourceId, Map<String, Abstract.Definition>> definitionIds = new HashMap<>();
+
+  // Name resolving
+  private final SimpleModuleNamespaceProvider moduleNsProvider  = new SimpleModuleNamespaceProvider();
+  private final SimpleStaticNamespaceProvider staticNsProvider  = new SimpleStaticNamespaceProvider();
+  private final DynamicNamespaceProvider dynamicNsProvider = new SimpleDynamicNamespaceProvider();
+
+  private final SourceInfoProvider<CompositeSourceSupplier<Prelude.SourceId, FileStorage.SourceId>.SourceId> srcInfoProvider;
+
+  // Typechecking
+  private final TypecheckerState state = new SimpleTypecheckerState();
+  private final Set<CompositeSourceSupplier<Prelude.SourceId, FileStorage.SourceId>.SourceId> requestedSources = new LinkedHashSet<>();
+
+
+  public ConsoleMain(CommandLine cmdLine) {
+    OneshotSourceInfoCollector<CompositeSourceSupplier<Prelude.SourceId, FileStorage.SourceId>.SourceId> srcInfoCollector = new OneshotSourceInfoCollector<>();
+    srcInfoProvider = srcInfoCollector.sourceInfoProvider;
+    errf = new ErrorFormatter(srcInfoProvider);
+
+    sourceDirStr = cmdLine.getOptionValue("s");
+    sourceDir = new File(sourceDirStr == null ? System.getProperty("user.dir") : sourceDirStr);
+    storage = createStorage(sourceDir, errorReporter);
+
     boolean recompile = cmdLine.hasOption("recompile");
+    ModuleLoadingListener moduleLoadingListener = new ModuleLoadingListener(srcInfoCollector);
+    moduleLoader = createModuleLoader(storage, moduleLoadingListener);
 
-    final SimpleStaticNamespaceProvider staticNsProvider = new SimpleStaticNamespaceProvider();
-    final DynamicNamespaceProvider dynamicNsProvider = new SimpleDynamicNamespaceProvider();
-    final ListErrorReporter errorReporter = new ListErrorReporter();
-    final SimpleModuleNamespaceProvider moduleNsProvider = new SimpleModuleNamespaceProvider();
-    final OneshotNameResolver oneshotNameResolver = new OneshotNameResolver(errorReporter, new ConcreteResolveListener(), moduleNsProvider, staticNsProvider, dynamicNsProvider);
-    final OneshotSourceInfoCollector<CompositeSourceSupplier<Prelude.SourceId, FileStorage.SourceId>.SourceId> srcInfoCollector = new OneshotSourceInfoCollector<>();
-    final ErrorFormatter errf = new ErrorFormatter(srcInfoCollector.sourceInfoProvider);
-    final List<CompositeSourceSupplier<Prelude.SourceId, FileStorage.SourceId>.SourceId> loadedModules = new ArrayList<>();
-    final List<Abstract.Definition> modulesToTypeCheck = new ArrayList<>();
-    final Map<CompositeSourceSupplier<Prelude.SourceId, FileStorage.SourceId>.SourceId, Map<String, Abstract.Definition>> definitionIds = new HashMap<>();
-
-    // Storage
-    final Prelude.PreludeStorage preludeStorage = new Prelude.PreludeStorage(errorReporter);
-    final FileStorage fileStorage = new FileStorage(sourceDir, errorReporter);
-    final CompositeStorage<Prelude.SourceId, FileStorage.SourceId> storage = new CompositeStorage<>(preludeStorage, fileStorage, preludeStorage, fileStorage);
-
-    class ModuleLoadingListener extends BaseModuleLoader.ModuleLoadingListener<CompositeSourceSupplier<Prelude.SourceId, FileStorage.SourceId>.SourceId> {
-      private ModuleLoader moduleLoader = null;
-      private Namespace preludeNamespace = new EmptyNamespace();
-
-      private void setModuleLoader(ModuleLoader moduleLoader) {
-        this.moduleLoader = moduleLoader;
-      }
-      private void setPreludeNamespace(Namespace preludeNamespace) {
-        this.preludeNamespace = preludeNamespace;
-      }
-
-      @Override
-      public void loadingSucceeded(CompositeSourceSupplier<Prelude.SourceId, FileStorage.SourceId>.SourceId module, Abstract.ClassDefinition abstractDefinition) {
-        if (abstractDefinition != null) {
-          if (!definitionIds.containsKey(module)) {
-            definitionIds.put(module, new HashMap<String, Abstract.Definition>());
-          }
-
-          new LoadModulesRecursively(moduleLoader).visitClass(abstractDefinition, null);
-
-          oneshotNameResolver.visitModule(abstractDefinition, preludeNamespace);
-          srcInfoCollector.visitModule(module, abstractDefinition);
-
-          modulesToTypeCheck.add(abstractDefinition);
-        }
-        moduleNsProvider.registerModule(module.getModulePath(), abstractDefinition);
-        loadedModules.add(module);
-        System.out.println("[Loaded] " + module.getModulePath());
-      }
-    }
-    ModuleLoadingListener moduleLoadingListener = new ModuleLoadingListener();
-
-    final BaseModuleLoader<CompositeSourceSupplier<Prelude.SourceId, FileStorage.SourceId>.SourceId> moduleLoader = new BaseModuleLoader<>(storage, moduleLoadingListener);
-    moduleLoadingListener.setModuleLoader(moduleLoader);
-
-    final Abstract.ClassDefinition prelude = moduleLoader.load(Prelude.PreludeStorage.PRELUDE_MODULE_PATH);
-    final Namespace preludeNamespace = staticNsProvider.forDefinition(prelude);
+    Namespace preludeNamespace = loadPrelude();
     moduleLoadingListener.setPreludeNamespace(preludeNamespace);
 
-    final TypecheckerState state = new SimpleTypecheckerState();
+    argFiles = cmdLine.getArgList();
+  }
 
+  private static CompositeStorage<Prelude.SourceId, FileStorage.SourceId> createStorage(File sourceDir, ErrorReporter errorReporter) {
+    Prelude.PreludeStorage preludeStorage = new Prelude.PreludeStorage(errorReporter);
+    FileStorage fileStorage = new FileStorage(sourceDir, errorReporter);
+    return new CompositeStorage<>(preludeStorage, fileStorage, preludeStorage, fileStorage);
+  }
+
+  private static SourceModuleLoader<CompositeSourceSupplier<Prelude.SourceId, FileStorage.SourceId>.SourceId> createModuleLoader(CompositeStorage<Prelude.SourceId, FileStorage.SourceId> storage, ModuleLoadingListener moduleLoadingListener) {
+    return new BaseModuleLoader<>(storage, moduleLoadingListener);
+  }
+
+  private Namespace loadPrelude() {
+    Abstract.ClassDefinition prelude = moduleLoader.load(Prelude.PreludeStorage.PRELUDE_MODULE_PATH);
     TypecheckingOrdering.typecheck(state, staticNsProvider, dynamicNsProvider, Collections.singletonList(prelude), new DummyErrorReporter(), true);
     assert errorReporter.getErrorList().isEmpty();
+    return staticNsProvider.forDefinition(prelude);
+  }
 
-
-    if (!errorReporter.getErrorList().isEmpty()) {
-      for (GeneralError error : errorReporter.getErrorList()) {
-        System.err.println(errf.printError(error));
-      }
-      return;
-    }
-
+  private void run() {
+    // Collect sources for which typechecking was requested
     final Path sourceDirPath = sourceDir.toPath();
-    if (cmdLine.getArgList().isEmpty()) {
+    if (argFiles.isEmpty()) {
       if (sourceDirStr == null) return;
       try {
         Files.walkFileTree(sourceDirPath, new SimpleFileVisitor<Path>() {
           @Override
           public FileVisitResult visitFile(Path path, BasicFileAttributes basicFileAttributes) throws IOException {
-            if (path.toString().endsWith(FileStorage.EXTENSION)) {
-              processFile(storage, moduleLoader, errorReporter, errf, sourceDirPath.relativize(path).toFile());
-            }
+            requestFileTypechecking(sourceDirPath.relativize(path).toFile());
             return FileVisitResult.CONTINUE;
           }
 
@@ -149,21 +126,78 @@ public class ConsoleMain {
         System.err.println(GeneralError.ioError(e));
       }
     } else {
-      for (String fileName : cmdLine.getArgList()) {
-        processFile(storage, moduleLoader, errorReporter, errf, new File(fileName));
+      for (String fileName : argFiles) {
+        requestFileTypechecking(new File(fileName));
       }
     }
 
-    final Map<SourceId, ModuleResult> failedModules = new HashMap<>();
+    // Typecheck those sources
+    Map<CompositeSourceSupplier<Prelude.SourceId, FileStorage.SourceId>.SourceId, ModuleResult> typeCheckResults = typeCheckSources(requestedSources);
 
-    TypecheckingOrdering.typecheck(state, staticNsProvider, dynamicNsProvider, modulesToTypeCheck, errorReporter, new TypecheckedReporter() {
+    flushErrors();
+
+    // Output nice per-module typechecking results
+    for (Map.Entry<CompositeSourceSupplier<Prelude.SourceId, FileStorage.SourceId>.SourceId, ModuleResult> entry : typeCheckResults.entrySet()) {
+      if (!requestedSources.contains(entry.getKey())) {
+        ModuleResult result = entry.getValue();
+        reportTypeCheckResult(entry.getKey(), result == ModuleResult.OK ? ModuleResult.UNKNOWN : result);
+      }
+    }
+    for (CompositeSourceSupplier<Prelude.SourceId, FileStorage.SourceId>.SourceId source : requestedSources) {
+      reportTypeCheckResult(source, typeCheckResults.get(source));
+    }
+
+    // TODO: Serialize typechecked modules
+  }
+
+  private void reportTypeCheckResult(CompositeSourceSupplier<Prelude.SourceId, FileStorage.SourceId>.SourceId source, ModuleResult result) {
+    StringBuilder builder = new StringBuilder();
+    builder.append("[").append(resultChar(result)).append("]");
+    builder.append(" ").append(displaySource(source, true));
+    System.out.println(builder);
+  }
+
+  private static char resultChar(ModuleResult result) {
+    switch (result) {
+      case NOT_LOADED:
+        return '✗';
+      case OK:
+        return '✓';
+      case GOALS:
+        return '◯';
+      case ERRORS:
+        return '✗';
+      default:
+        return ' ';
+    }
+  }
+
+  private Map<CompositeSourceSupplier<Prelude.SourceId, FileStorage.SourceId>.SourceId, ModuleResult> typeCheckSources(Set<CompositeSourceSupplier<Prelude.SourceId, FileStorage.SourceId>.SourceId> sources) {
+    final Map<CompositeSourceSupplier<Prelude.SourceId, FileStorage.SourceId>.SourceId, ModuleResult> results = new LinkedHashMap<>();
+
+    final Set<Abstract.ClassDefinition> modulesToTypeCheck = new LinkedHashSet<>();
+    for (CompositeSourceSupplier<Prelude.SourceId, FileStorage.SourceId>.SourceId source : sources) {
+      SourceSupplier.Result loadingResult = moduleLoader.load(source);
+      flushErrors();
+      if (loadingResult == null || loadingResult.definition == null) {
+        results.put(source, ModuleResult.NOT_LOADED);
+        continue;
+      }
+      modulesToTypeCheck.add(loadingResult.definition);
+    }
+
+    System.out.println("--- Checking ---");
+
+    TypecheckingOrdering.typecheck(state, staticNsProvider, dynamicNsProvider, new ArrayList<>(modulesToTypeCheck), errorReporter, new TypecheckedReporter() {
       @Override
       public void typecheckingSucceeded(Abstract.Definition definition) {
-
+        CompositeSourceSupplier<Prelude.SourceId, FileStorage.SourceId>.SourceId source = srcInfoProvider.sourceOf(definition);
+        updateModuleResult(source, ModuleResult.OK);
       }
 
       @Override
       public void typecheckingFailed(Abstract.Definition definition) {
+        CompositeSourceSupplier<Prelude.SourceId, FileStorage.SourceId>.SourceId source = srcInfoProvider.sourceOf(definition);
         boolean goals = false;
         boolean errors = false;
         for (GeneralError error : errorReporter.getErrorList()) {
@@ -173,54 +207,102 @@ public class ConsoleMain {
           if (error.getLevel() == Error.Level.ERROR) {
             errors = true;
           }
-          System.err.println(errf.printError(error));
         }
+        assert errors || goals;
+        results.put(source, errors ? ModuleResult.ERRORS : ModuleResult.GOALS);
 
-        if (errors || goals) {
-          failedModules.put(srcInfoCollector.sourceInfoProvider.sourceOf(definition), errors ? ModuleResult.ERRORS : ModuleResult.GOALS);
+        flushErrors();
+      }
+
+      private void updateModuleResult(CompositeSourceSupplier<Prelude.SourceId, FileStorage.SourceId>.SourceId source, ModuleResult result) {
+        ModuleResult prevResult = results.get(source);
+        if (prevResult == null || result.ordinal() > prevResult.ordinal()) {
+          results.put(source, result);
         }
-        errorReporter.getErrorList().clear();
       }
     }, false);
 
-    for (CompositeSourceSupplier<Prelude.SourceId, FileStorage.SourceId>.SourceId moduleId : loadedModules) {
-      StringBuilder builder = new StringBuilder();
-      ModuleResult result = failedModules.get(moduleId);
-      builder.append("[").append(result == ModuleResult.ERRORS ? "✗" : result == ModuleResult.GOALS ? "o" : " ").append("]");
-      builder.append(" ").append(moduleId.getModulePath());
-      if (moduleId.source2 != null) {
-        builder.append(" (").append(moduleId.source2).append(")");
-      }
-      System.out.println(builder);
-    }
-
-    for (GeneralError error : errorReporter.getErrorList()) {
-      System.err.println(errf.printError(error));
-    }
-
-    // TODO: Serialize typechecked modules
+    return results;
   }
 
-  private enum ModuleResult { GOALS, ERRORS, OK }
-
-  static private <SourceIdT extends SourceId> void processFile(SourceSupplier<SourceIdT> storage, SourceModuleLoader<SourceIdT> moduleLoader, ListErrorReporter errorReporter, ErrorFormatter errf, File file) {
-    if (!file.getName().endsWith(FileStorage.EXTENSION)) return;
-    String fileName = file.toString();
-    fileName = fileName.substring(0, fileName.length() - FileStorage.EXTENSION.length());
-
-    SourceIdT sourceId = storage.locateModule(FileStorage.modulePath(fileName));
-    if (sourceId == null) {
-      System.err.println(file + ": incorrect file name");
-      return;
-    }
-
-    moduleLoader.load(sourceId);
-
+  private void flushErrors() {
     for (GeneralError error : errorReporter.getErrorList()) {
-      System.err.println(errf.printError(error));
+      System.out.println(errf.printError(error));
     }
     errorReporter.getErrorList().clear();
   }
 
+  private enum ModuleResult { UNKNOWN, OK, GOALS, NOT_LOADED, ERRORS }
+
+  class ModuleLoadingListener extends BaseModuleLoader.ModuleLoadingListener<CompositeSourceSupplier<Prelude.SourceId, FileStorage.SourceId>.SourceId> {
+    private final OneshotNameResolver oneshotNameResolver = new OneshotNameResolver(errorReporter, new ConcreteResolveListener(), moduleNsProvider, staticNsProvider, dynamicNsProvider);
+    private final OneshotSourceInfoCollector<CompositeSourceSupplier<Prelude.SourceId, FileStorage.SourceId>.SourceId> mySrcInfoCollector;
+    private Namespace preludeNamespace = new EmptyNamespace();
+
+    ModuleLoadingListener(OneshotSourceInfoCollector<CompositeSourceSupplier<Prelude.SourceId, FileStorage.SourceId>.SourceId> srcInfoCollector) {
+      mySrcInfoCollector = srcInfoCollector;
+    }
+
+    void setPreludeNamespace(Namespace preludeNamespace) {
+      this.preludeNamespace = preludeNamespace;
+    }
+
+    @Override
+    public void loadingSucceeded(CompositeSourceSupplier<Prelude.SourceId, FileStorage.SourceId>.SourceId module, Abstract.ClassDefinition abstractDefinition) {
+      if (abstractDefinition != null) {
+        if (!definitionIds.containsKey(module)) {
+          definitionIds.put(module, new HashMap<String, Abstract.Definition>());
+        }
+
+        new LoadModulesRecursively(moduleLoader).visitClass(abstractDefinition, null);
+
+        oneshotNameResolver.visitModule(abstractDefinition, preludeNamespace);
+        mySrcInfoCollector.visitModule(module, abstractDefinition);
+      }
+      moduleNsProvider.registerModule(module.getModulePath(), abstractDefinition);
+      System.out.println("[Loaded] " + displaySource(module, false));
+    }
+  }
+
+  private static String displaySource(CompositeSourceSupplier<Prelude.SourceId, FileStorage.SourceId>.SourceId source, boolean modulePathOnly) {
+    StringBuilder builder = new StringBuilder();
+    builder.append(source.getModulePath());
+    if (!modulePathOnly && source.source2 != null) {
+      builder.append(" (").append(source.source2).append(")");
+    }
+    return builder.toString();
+  }
+
+  private void requestFileTypechecking(File file) {
+    if (!file.getName().endsWith(FileStorage.EXTENSION)) return;
+    String fileName = file.toString();
+    fileName = fileName.substring(0, fileName.length() - FileStorage.EXTENSION.length());
+
+    CompositeSourceSupplier<Prelude.SourceId, FileStorage.SourceId>.SourceId sourceId = storage.locateModule(FileStorage.modulePath(fileName));
+    if (sourceId == null) {
+      System.err.println(file + ": incorrect file name");
+    } else {
+      requestedSources.add(sourceId);
+    }
+  }
+
+
+  public static void main(String[] args) {
+    try {
+      CommandLine cmdLine = new DefaultParser().parse(cmdOptions, args);
+
+      if (cmdLine.hasOption("h")) {
+        printHelp();
+      } else {
+        new ConsoleMain(cmdLine).run();
+      }
+    } catch (ParseException e) {
+      System.err.println(e.getMessage());
+    }
+  }
+
+  private static void printHelp() {
+    new HelpFormatter().printHelp("vclang [FILES]", cmdOptions);
+  }
 
 }
