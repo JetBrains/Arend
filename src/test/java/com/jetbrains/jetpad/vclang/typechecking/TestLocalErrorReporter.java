007771d5d2d903c6a80ee17dddd29532a93bc112
Kirill Elagin <kirelagin@gmail.com>
2017-05-26 19:11:14 +0300

    Tests: fake definition for reporting errors

--- src/test/java/com/jetbrains/jetpad/vclang/typechecking/TestLocalErrorReporter.java
+++ src/test/java/com/jetbrains/jetpad/vclang/typechecking/TestLocalErrorReporter.java
@@ -2,12 +2,17 @@ package com.jetbrains.jetpad.vclang.typechecking;
 
 import com.jetbrains.jetpad.vclang.error.ErrorReporter;
 import com.jetbrains.jetpad.vclang.error.GeneralError;
+import com.jetbrains.jetpad.vclang.frontend.Concrete;
+import com.jetbrains.jetpad.vclang.term.Abstract;
 import com.jetbrains.jetpad.vclang.typechecking.error.LocalErrorReporter;
 import com.jetbrains.jetpad.vclang.typechecking.error.TypeCheckingError;
 import com.jetbrains.jetpad.vclang.typechecking.error.local.LocalTypeCheckingError;
 
+import java.util.Collections;
+
 public class TestLocalErrorReporter implements LocalErrorReporter {
   private final ErrorReporter errorReporter;
+  private final Abstract.Definition fakeDef = new Concrete.FunctionDefinition(null, "testDef", Abstract.Precedence.DEFAULT, Collections.emptyList(), null, Abstract.Definition.Arrow.RIGHT, null, Collections.emptyList());
 
   public TestLocalErrorReporter(ErrorReporter errorReporter) {
     this.errorReporter = errorReporter;
@@ -15,7 +20,7 @@ public class TestLocalErrorReporter implements LocalErrorReporter {
 
   @Override
   public void report(LocalTypeCheckingError localError) {
-    errorReporter.report(new TypeCheckingError(null, localError));
+    errorReporter.report(new TypeCheckingError(fakeDef, localError));
   }
 
   @Override
