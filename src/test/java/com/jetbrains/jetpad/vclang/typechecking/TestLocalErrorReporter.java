9867a26a85f982cd72e89a370e0410cede031c7b
Kirill Elagin <kirelagin@gmail.com>
2017-05-26 19:14:30 +0300

    tests: even beter fake definniton for tests

--- src/test/java/com/jetbrains/jetpad/vclang/typechecking/TestLocalErrorReporter.java
+++ src/test/java/com/jetbrains/jetpad/vclang/typechecking/TestLocalErrorReporter.java
@@ -2,17 +2,40 @@ package com.jetbrains.jetpad.vclang.typechecking;
 
 import com.jetbrains.jetpad.vclang.error.ErrorReporter;
 import com.jetbrains.jetpad.vclang.error.GeneralError;
-import com.jetbrains.jetpad.vclang.frontend.Concrete;
 import com.jetbrains.jetpad.vclang.term.Abstract;
+import com.jetbrains.jetpad.vclang.term.AbstractDefinitionVisitor;
 import com.jetbrains.jetpad.vclang.typechecking.error.LocalErrorReporter;
 import com.jetbrains.jetpad.vclang.typechecking.error.TypeCheckingError;
 import com.jetbrains.jetpad.vclang.typechecking.error.local.LocalTypeCheckingError;
 
-import java.util.Collections;
-
 public class TestLocalErrorReporter implements LocalErrorReporter {
   private final ErrorReporter errorReporter;
-  private final Abstract.Definition fakeDef = new Concrete.FunctionDefinition(null, "testDef", Abstract.Precedence.DEFAULT, Collections.emptyList(), null, Abstract.Definition.Arrow.RIGHT, null, Collections.emptyList());
+  private final Abstract.Definition fakeDef = new Abstract.Definition() {
+    @Override
+    public Abstract.Precedence getPrecedence() {
+      return null;
+    }
+
+    @Override
+    public Abstract.Definition getParentDefinition() {
+      return null;
+    }
+
+    @Override
+    public boolean isStatic() {
+      return true;
+    }
+
+    @Override
+    public <P, R> R accept(AbstractDefinitionVisitor<? super P, ? extends R> visitor, P params) {
+      return null;
+    }
+
+    @Override
+    public String getName() {
+      return "testDefinition";
+    }
+  };
 
   public TestLocalErrorReporter(ErrorReporter errorReporter) {
     this.errorReporter = errorReporter;
