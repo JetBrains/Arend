0c71a2761f00f5fa11a56795fd40460953e131a0
valis <valery.isaev@gmail.com>
2017-08-11 17:33:37 +0300

    Rewrite the error message of termination checker

--- src/main/java/com/jetbrains/jetpad/vclang/typechecking/error/local/TerminationCheckError.java
+++ src/main/java/com/jetbrains/jetpad/vclang/typechecking/error/local/TerminationCheckError.java
@@ -2,64 +2,39 @@ package com.jetbrains.jetpad.vclang.typechecking.error.local;
 
 import com.jetbrains.jetpad.vclang.core.definition.Definition;
 import com.jetbrains.jetpad.vclang.error.GeneralError;
+import com.jetbrains.jetpad.vclang.error.doc.Doc;
+import com.jetbrains.jetpad.vclang.error.doc.LineDoc;
 import com.jetbrains.jetpad.vclang.term.Abstract;
-import com.jetbrains.jetpad.vclang.typechecking.termination.BaseCallMatrix;
+import com.jetbrains.jetpad.vclang.term.SourceInfoProvider;
 import com.jetbrains.jetpad.vclang.typechecking.termination.CompositeCallMatrix;
 import com.jetbrains.jetpad.vclang.typechecking.termination.RecursiveBehavior;
 
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.jetbrains.jetpad.vclang.error.doc.DocFactory.*;
 
 public class TerminationCheckError extends GeneralError {
   public final Abstract.Definition definition;
+  public final Set<RecursiveBehavior<Definition>> behaviors;
 
   public TerminationCheckError(Definition def, Set<RecursiveBehavior<Definition>> behaviors) {
-    super(formErrorMessage(def, behaviors), def.getAbstractDefinition());
+    super("", def.getAbstractDefinition());
     definition = def.getAbstractDefinition();
+    this.behaviors = behaviors;
   }
 
-  private static <T> String formErrorMessage(Definition def, Set<RecursiveBehavior<T>> behaviors) {
-    StringBuilder builder = new StringBuilder("Termination check failed for function ");
-    builder.append(def.getName());
-    if (!behaviors.isEmpty()) {
-      builder.append('\n');
-      for (RecursiveBehavior rb : behaviors) {
-        builder.append(printBehavior(rb));
-      }
-    }
-    return builder.toString();
+  @Override
+  public LineDoc getHeaderDoc(SourceInfoProvider src) {
+    return hList(super.getHeaderDoc(src), text("Termination check failed for function '"), refDoc(definition), text("'"));
   }
 
-  private static String printBehavior(RecursiveBehavior rb) {
-    StringBuilder result = new StringBuilder();
-    String brace = "";
-    if (rb.myInitialCallMatrix instanceof CompositeCallMatrix) {
-      result.append("Composite recursive call: {\n ");
-      brace = "}";
-    } else {
-      result.append("Recursive call: ");
-    }
-    result.append(rb.myInitialCallMatrix.getMatrixLabel()).append(brace);
+  @Override
+  public Doc getBodyDoc(SourceInfoProvider src) {
+    return vList(behaviors.stream().map(TerminationCheckError::printBehavior).collect(Collectors.toList()));
+  }
 
-    StringBuilder unknown = new StringBuilder();
-    int unknownCount = 0;
-    StringBuilder equal = new StringBuilder();
-    int equalCount = 0;
-    for (int i = 0; i < rb.getLength(); i++) {
-      if (rb.myBehavior.get(i) == BaseCallMatrix.R.Unknown) {
-        unknown.append(unknown.length() > 0 ? ", " : "").append(rb.myLabels.get(i));
-        unknownCount++;
-      } else if (rb.myBehavior.get(i) == BaseCallMatrix.R.Equal) {
-        equal.append(equal.length() > 0 ? ", " : "").append(rb.myLabels.get(i));
-        equalCount++;
-      }
-    }
-    if (unknown.length() > 0) {
-      result.append("\nUnknown recursive behavior for argument").append(unknownCount > 1 ? "s" : "").append(": ").append(unknown);
-    }
-    if (equal.length() > 0) {
-      result.append("\nDoes not strictly decrease on argument").append(equalCount > 1 ? "s" : "").append(": ").append(equal);
-    }
-
-    return result.toString();
+  private static Doc printBehavior(RecursiveBehavior rb) {
+    return hang(text(rb.initialCallMatrix instanceof CompositeCallMatrix ? "Problematic sequence of recursive calls:" : "Problematic recursive call:"), rb.initialCallMatrix.getMatrixLabel());
   }
 }
--- src/main/java/com/jetbrains/jetpad/vclang/typechecking/termination/RecursiveBehavior.java
+++ src/main/java/com/jetbrains/jetpad/vclang/typechecking/termination/RecursiveBehavior.java
@@ -7,37 +7,37 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class RecursiveBehavior<T> {
-  public List<BaseCallMatrix.R> myBehavior;
-  public List<String> myLabels;
-  public BaseCallMatrix<T> myInitialCallMatrix;
+  public final List<BaseCallMatrix.R> behavior;
+  public final List<String> labels;
+  public final BaseCallMatrix<T> initialCallMatrix;
 
   RecursiveBehavior(BaseCallMatrix<T> callMatrix) {
-    myBehavior = new LinkedList<>();
-    for (int i = 0; i < callMatrix.getHeight(); i++) myBehavior.add(callMatrix.getValue(i, i));
-    myLabels = new LinkedList<>(Arrays.asList(callMatrix.getRowLabels()));
-    if (myLabels.size() != myBehavior.size())
+    behavior = new LinkedList<>();
+    for (int i = 0; i < callMatrix.getHeight(); i++) behavior.add(callMatrix.getValue(i, i));
+    labels = new LinkedList<>(Arrays.asList(callMatrix.getRowLabels()));
+    if (labels.size() != behavior.size())
       throw new IllegalArgumentException();
-    myInitialCallMatrix = callMatrix;
+    initialCallMatrix = callMatrix;
   }
 
   RecursiveBehavior(RecursiveBehavior<T> rb, int i) {
     if (i < 0 || i >= rb.getLength()) throw new IllegalArgumentException();
-    myBehavior = new LinkedList<>(rb.myBehavior);
-    myLabels = new LinkedList<>(rb.myLabels);
-    myBehavior.remove(i);
-    myLabels.remove(i);
-    myInitialCallMatrix = rb.myInitialCallMatrix;
+    behavior = new LinkedList<>(rb.behavior);
+    labels = new LinkedList<>(rb.labels);
+    behavior.remove(i);
+    labels.remove(i);
+    initialCallMatrix = rb.initialCallMatrix;
   }
 
   public int getLength() {
-    return myBehavior.size();
+    return behavior.size();
   }
 
   @Override
   public boolean equals(Object o) {
     if (o instanceof RecursiveBehavior) {
       RecursiveBehavior rb2 = (RecursiveBehavior) o;
-      return myBehavior.equals(rb2.myBehavior) && myLabels.equals(rb2.myLabels);
+      return behavior.equals(rb2.behavior) && labels.equals(rb2.labels);
     }
     return false;
   }
@@ -45,9 +45,9 @@ public class RecursiveBehavior<T> {
   boolean leq(RecursiveBehavior<T> r) {
     if (getLength() != r.getLength()) throw new IllegalArgumentException();
     for (int i = 0; i < getLength(); i++)
-      if (!CallMatrix.rleq(myBehavior.get(i), r.myBehavior.get(i)))
+      if (!CallMatrix.rleq(behavior.get(i), r.behavior.get(i)))
         return false;
-    return !this.equals(r) || myInitialCallMatrix.getCompositeLength() <= r.myInitialCallMatrix.getCompositeLength();
+    return !this.equals(r) || initialCallMatrix.getCompositeLength() <= r.initialCallMatrix.getCompositeLength();
   }
 
   @Override
@@ -55,9 +55,9 @@ public class RecursiveBehavior<T> {
     StringBuilder labels = new StringBuilder();
     StringBuilder values = new StringBuilder();
     for (int i = 0; i < getLength(); i++) {
-      String label = myLabels.get(i);
+      String label = this.labels.get(i);
       labels.append(' ').append(label);
-      CallMatrix.R r = myBehavior.get(i);
+      CallMatrix.R r = behavior.get(i);
       values.append(StringFormat.rightPad(label.length() + 1, CallMatrix.rToChar(r)));
     }
     return labels.toString() + '\n' + values.toString();
