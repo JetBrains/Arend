cfc4e6a8f1750d7647c2721b5a51f50f34732b56
Kirill Elagin <kirelagin@gmail.com>
2017-10-13 20:24:13 +0300

    Fix crash on reporting a ParseError

--- src/main/java/com/jetbrains/jetpad/vclang/frontend/parser/ParserError.java
+++ src/main/java/com/jetbrains/jetpad/vclang/frontend/parser/ParserError.java
@@ -1,13 +1,15 @@
 package com.jetbrains.jetpad.vclang.frontend.parser;
 
 import com.jetbrains.jetpad.vclang.error.GeneralError;
+import com.jetbrains.jetpad.vclang.module.source.SourceId;
 import com.jetbrains.jetpad.vclang.naming.reference.GlobalReferable;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.function.Function;
 
 public class ParserError extends GeneralError {
-  public final Position position;
+  public Position position;
 
   public ParserError(Position position, String message) {
     super(Level.ERROR, message);
@@ -22,5 +24,9 @@ public class ParserError extends GeneralError {
   @Override
   public Collection<? extends GlobalReferable> getAffectedDefinitions() {
     return Collections.singletonList(new SourceIdReference(position.module));
+  }
+
+  public void fixup(Function<SourceId, SourceId> fix) {
+    position = new Position(fix.apply(position.module), position.line, position.column);
   }
 }
