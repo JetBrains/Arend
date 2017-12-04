4dd2732bab3e15b366d238e30cad2c801cf519d5
valis <valery.isaev@gmail.com>
2017-12-04 16:53:59 +0300

    Cache null values in CachingConcreteProvider

--- src/main/java/com/jetbrains/jetpad/vclang/typechecking/typecheckable/provider/CachingConcreteProvider.java
+++ src/main/java/com/jetbrains/jetpad/vclang/typechecking/typecheckable/provider/CachingConcreteProvider.java
@@ -2,6 +2,7 @@ package com.jetbrains.jetpad.vclang.typechecking.typecheckable.provider;
 
 import com.jetbrains.jetpad.vclang.naming.reference.GlobalReferable;
 import com.jetbrains.jetpad.vclang.term.concrete.Concrete;
+import com.jetbrains.jetpad.vclang.term.concrete.ConcreteDefinitionVisitor;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -9,6 +10,12 @@ import java.util.Map;
 public class CachingConcreteProvider implements ConcreteProvider {
   private ConcreteProvider myProvider;
   private final Map<GlobalReferable, Concrete.ReferableDefinition> myCache = new HashMap<>();
+  private final static Concrete.ReferableDefinition NULL_INSTANCE = new Concrete.Definition(null) {
+    @Override
+    public <P, R> R accept(ConcreteDefinitionVisitor<? super P, ? extends R> visitor, P params) {
+      return null;
+    }
+  };
 
   public CachingConcreteProvider() {
     myProvider = EmptyConcreteProvider.INSTANCE;
@@ -26,13 +33,11 @@ public class CachingConcreteProvider implements ConcreteProvider {
   public Concrete.ReferableDefinition getConcrete(GlobalReferable referable) {
     Concrete.ReferableDefinition definition = myCache.get(referable);
     if (definition != null) {
-      return definition;
+      return definition == NULL_INSTANCE ? null : definition;
     }
 
     definition = myProvider.getConcrete(referable);
-    if (definition != null) {
-      myCache.put(referable, definition);
-    }
+    myCache.put(referable, definition == null ? NULL_INSTANCE : definition);
     return definition;
   }
 
