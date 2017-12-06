7dd76672a71a386836a64adf9500c3a933a9c40f
valis <valery.isaev@gmail.com>
2017-12-06 19:26:03 +0300

    Add tests on name resolving and coercing for classes

--- src/test/java/com/jetbrains/jetpad/vclang/classes/CoerceTest.java
+++ src/test/java/com/jetbrains/jetpad/vclang/classes/CoerceTest.java
@@ -15,6 +15,16 @@ public class CoerceTest extends TypeCheckingTestCase {
   }
 
   @Test
+  public void coerceType() {
+    typeCheckModule(
+      "\\class C (A : \\Set) {\n" +
+      "  | a : A\n" +
+      "}\n" +
+      "\\function f (c : C) : c => Nat\n" +
+      "\\function g (n : f (\\new C { | A => Nat | a => 0 })) => 1");
+  }
+
+  @Test
   public void doNotCoerce() {
     typeCheckModule(
       "\\class C (A : \\Set) {\n" +
@@ -35,9 +45,19 @@ public class CoerceTest extends TypeCheckingTestCase {
   @Test
   public void doNotCoerceSuperMultiple() {
     typeCheckModule(
-      "\\class C (A : Nat)\n" +
+      "\\class C1 (A : Nat)\n" +
       "\\class C2 (B : \\Set)\n" +
-      "\\class D \\extends C, C2 { | b : B }\n" +
+      "\\class D \\extends C1, C2 { | b : B }\n" +
       "\\function f (d : D) : \\Set => d");
+  }
+
+  @Test
+  public void coerceExtends() {
+    typeCheckModule(
+      "\\class C1 (n : Nat)\n" +
+      "\\class C2 (B : \\Set)\n" +
+      "\\class D (n : Nat) \\extends C2 \\Prop, C1 n { | b : B }\n" +
+      "\\function f (d : D) : d => b d\n" +
+      "\\function g (d : D) : Nat => n d");
   }
 }
