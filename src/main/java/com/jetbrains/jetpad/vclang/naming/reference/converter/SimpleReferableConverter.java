07c9f98d0cee8325c036723cbd95b695088072ef
valis <valery.isaev@gmail.com>
2018-04-24 14:06:09 +0300

    Revert "Replace FullName references with weak references to psi elements"
    This reverts commit 68ea481fcf1a909cef5907ff62c32f607a232a83.

--- src/main/java/com/jetbrains/jetpad/vclang/naming/reference/converter/SimpleReferableConverter.java
+++ src/main/java/com/jetbrains/jetpad/vclang/naming/reference/converter/SimpleReferableConverter.java
@@ -3,13 +3,14 @@ package com.jetbrains.jetpad.vclang.naming.reference.converter;
 import com.jetbrains.jetpad.vclang.naming.reference.LocatedReferable;
 import com.jetbrains.jetpad.vclang.naming.reference.Referable;
 import com.jetbrains.jetpad.vclang.naming.reference.TCReferable;
+import com.jetbrains.jetpad.vclang.util.FullName;
 
+import java.util.HashMap;
 import java.util.Map;
-import java.util.WeakHashMap;
 import java.util.function.Function;
 
 public class SimpleReferableConverter implements ReferableConverter {
-  private final Map<LocatedReferable, TCReferable> myMap = new WeakHashMap<>();
+  private final Map<FullName, TCReferable> myMap = new HashMap<>();
 
   @Override
   public Referable toDataReferable(Referable referable) {
@@ -18,15 +19,15 @@ public class SimpleReferableConverter implements ReferableConverter {
 
   @Override
   public TCReferable toDataLocatedReferable(LocatedReferable referable) {
-    return referable == null ? null : myMap.get(referable);
+    return referable == null ? null : myMap.get(new FullName(referable));
   }
 
-  public TCReferable computeIfAbsent(LocatedReferable referable, Function<? super LocatedReferable, ? extends TCReferable> tcReferable) {
-    return myMap.computeIfAbsent(referable, tcReferable);
+  public TCReferable computeIfAbsent(LocatedReferable referable, Function<? super FullName, ? extends TCReferable> tcReferable) {
+    return myMap.computeIfAbsent(new FullName(referable), tcReferable);
   }
 
   public TCReferable remove(LocatedReferable referable) {
-    return myMap.remove(referable);
+    return myMap.remove(new FullName(referable));
   }
 
   public void clear() {
