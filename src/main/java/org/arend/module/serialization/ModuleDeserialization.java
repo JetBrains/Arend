a86de98b68820162a77bc070549cd72764e6afec
Valery Isaev <valery.isaev@jetbrains.com>
2019-09-07 10:15:13 +0300

    Fix a bug in deserialization

--- src/main/java/org/arend/module/serialization/ModuleDeserialization.java
+++ src/main/java/org/arend/module/serialization/ModuleDeserialization.java
@@ -265,6 +265,9 @@ public class ModuleDeserialization {
     final Definition def;
     switch (defProto.getDefinitionDataCase()) {
       case CLASS:
+        if (!(referable instanceof TCClassReferable)) {
+          throw new DeserializationException("'" + referable.textRepresentation() + "' expected to be a class");
+        }
         ClassDefinition classDef = new ClassDefinition((TCClassReferable) referable);
         if (fillInternalDefinitions) {
           for (DefinitionProtos.Definition.ClassData.Field fieldProto : defProto.getClass_().getPersonalFieldList()) {
