e03baa61dc3e7f65bea163cd03d84086108be019
sxhya <sxh@ya.ru>
2019-10-26 03:56:33 +0300

    Removed modification of print options

--- src/main/java/org/arend/typechecking/error/local/GoalError.java
+++ src/main/java/org/arend/typechecking/error/local/GoalError.java
@@ -3,16 +3,12 @@ package org.arend.typechecking.error.local;
 import org.arend.core.context.binding.Binding;
 import org.arend.core.expr.Expression;
 import org.arend.core.expr.type.ExpectedType;
-import org.arend.core.expr.visitor.ToAbstractVisitor;
 import org.arend.error.GeneralError;
 import org.arend.error.doc.Doc;
 import org.arend.naming.reference.Referable;
 import org.arend.term.concrete.Concrete;
 import org.arend.term.prettyprint.PrettyPrinterConfig;
-
 import java.util.*;
-
-import static org.arend.core.expr.visitor.ToAbstractVisitor.Flag.*;
 import static org.arend.error.doc.DocFactory.*;
 
 public class GoalError extends TypecheckingError {
@@ -33,19 +29,6 @@ public class GoalError extends TypecheckingError {
 
   @Override
   public Doc getBodyDoc(PrettyPrinterConfig ppConfig) {
-    EnumSet<ToAbstractVisitor.Flag> flags = ppConfig.getExpressionFlags().clone();
-    flags.remove(SHOW_CON_PARAMS);
-    flags.remove(SHOW_IMPLICIT_ARGS);
-    flags.remove(SHOW_TYPES_IN_LAM);
-    flags.remove(SHOW_INFERENCE_LEVEL_VARS);
-    flags.add(HIDE_HIDEABLE_DEFINITIONS);
-    ppConfig = new PrettyPrinterConfig() {
-      @Override
-      public EnumSet<ToAbstractVisitor.Flag> getExpressionFlags() {
-        return flags;
-      }
-    };
-
     Doc expectedDoc = expectedType == null ? nullDoc() : hang(text("Expected type:"), expectedType.prettyPrint(ppConfig));
     Doc actualDoc = actualType == null ? nullDoc() : hang(text(expectedType != null ? "  Actual type:" : "Type:"), termDoc(actualType, ppConfig));
 
