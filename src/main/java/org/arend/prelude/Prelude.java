b0365880a48bc6818d40a0c7a55aa178c4e472b1
ice1000 <ice1000kotlin@foxmail.com>
2020-01-21 13:52:00 -0500

    Generate Java class containing the latest version

--- src/main/java/org/arend/prelude/Prelude.java
+++ src/main/java/org/arend/prelude/Prelude.java
@@ -35,7 +35,7 @@ import static org.arend.core.expr.ExpressionFactory.Nat;
 import static org.arend.core.expr.ExpressionFactory.parameter;
 
 public class Prelude {
-  public static final String VERSION = "1.1";
+  public static final String VERSION = GeneratedVersion.VERSION;
 
   public static final ModulePath MODULE_PATH = new ModulePath("Prelude");
   public static final String LIBRARY_NAME = "prelude";
