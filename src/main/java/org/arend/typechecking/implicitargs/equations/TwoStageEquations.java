e446367d8ecb4e16111a08e3c8f0a11d51115525
valis <valery.isaev@gmail.com>
2020-02-04 21:35:13 +0300

    Fix a bug with level equations

--- src/main/java/org/arend/typechecking/implicitargs/equations/TwoStageEquations.java
+++ src/main/java/org/arend/typechecking/implicitargs/equations/TwoStageEquations.java
@@ -349,6 +349,9 @@ public class TwoStageEquations implements Equations {
 
     if (cmp == CMP.LE || cmp == CMP.EQ) {
       addLevelEquation(level1.getVar(), level2.getVar(), level2.getConstant() - level1.getConstant(), level2.getMaxAddedConstant() - level1.getConstant(), sourceNode);
+      if (level1.withMaxConstant() && level1.getMaxAddedConstant() > level2.getMaxAddedConstant()) {
+        addLevelEquation(null, level2.getVar(), level2.getConstant() - level1.getMaxAddedConstant(), -1, sourceNode);
+      }
       /*
       if (level1.getVar() != null || level1.getConstant() != 0) {
         addLevelEquation(null, level2.getVar(), level2.getConstant() - level1.getMaxAddedConstant(), level2.getMaxAddedConstant() - level1.getMaxAddedConstant(), sourceNode);
@@ -357,6 +360,9 @@ public class TwoStageEquations implements Equations {
     }
     if (cmp == CMP.GE || cmp == CMP.EQ) {
       addLevelEquation(level2.getVar(), level1.getVar(), level1.getConstant() - level2.getConstant(), level1.getMaxAddedConstant() - level2.getConstant(), sourceNode);
+      if (level2.withMaxConstant() && level2.getMaxAddedConstant() > level1.getMaxAddedConstant()) {
+        addLevelEquation(null, level1.getVar(), level1.getConstant() - level2.getMaxAddedConstant(), -1, sourceNode);
+      }
       /*
       if (level2.getVar() != null || level2.getConstant() != 0) {
         addLevelEquation(null, level1.getVar(), level1.getConstant() - level2.getMaxAddedConstant(), level1.getMaxAddedConstant() - level2.getMaxAddedConstant(), sourceNode);
