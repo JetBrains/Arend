3f9e12230629e51c0c446569b41d550e8e94c5e5
ice1000 <ice1000kotlin@foxmail.com>
2020-02-29 01:39:26 -0500

    Test implemented parts of the subDef visitor

---
+++ src/test/java/org/arend/typechecking/subexpr/CorrespondedSubDefTest.java
@@ -0,0 +1,42 @@
+package org.arend.typechecking.subexpr;
+
+import org.arend.core.expr.Expression;
+import org.arend.frontend.reference.ConcreteLocatedReferable;
+import org.arend.term.concrete.Concrete;
+import org.arend.typechecking.TypeCheckingTestCase;
+import org.arend.util.Pair;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CorrespondedSubDefTest extends TypeCheckingTestCase {
+  @Test
+  public void funBody() {
+    ConcreteLocatedReferable referable = resolveNamesDef("\\func f => 0");
+    Concrete.FunctionDefinition def = (Concrete.FunctionDefinition) referable.getDefinition();
+    assertNotNull(def.getBody());
+    Pair<Expression, Concrete.Expression> accept = def.accept(
+        new CorrespondedSubDefVisitor(def.getBody().getTerm()), typeCheckDef(referable));
+    assertEquals("0", accept.proj1.toString());
+  }
+
+  @Test
+  public void funResultType() {
+    ConcreteLocatedReferable referable = resolveNamesDef("\\func f : Nat => 0");
+    Concrete.FunctionDefinition def = (Concrete.FunctionDefinition) referable.getDefinition();
+    assertNotNull(def.getResultType());
+    Pair<Expression, Concrete.Expression> accept = def.accept(
+        new CorrespondedSubDefVisitor(def.getResultType()), typeCheckDef(referable));
+    assertEquals("Nat", accept.proj1.toString());
+  }
+
+  @Test
+  public void funParamType() {
+    ConcreteLocatedReferable referable = resolveNamesDef("\\func f (a : Nat) => a");
+    Concrete.FunctionDefinition def = (Concrete.FunctionDefinition) referable.getDefinition();
+    assertFalse(def.getParameters().isEmpty());
+    Pair<Expression, Concrete.Expression> accept = def.accept(
+        new CorrespondedSubDefVisitor(def.getParameters().get(0).getType()), typeCheckDef(referable));
+    assertEquals("Nat", accept.proj1.toString());
+  }
+}
