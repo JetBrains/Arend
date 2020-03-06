8544ab95e5f1d448b584213621cd1f65744ebbfd
ice1000 <ice1000kotlin@foxmail.com>
2020-03-05 20:52:33 -0500

    Add Arend.java & ArendExpr.java

---
+++ src/main/java/org/arend/util/ArendExpr.java
@@ -0,0 +1,15 @@
+package org.arend.util;
+
+import org.intellij.lang.annotations.Language;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+
+@Retention(RetentionPolicy.CLASS)
+@Target({METHOD, FIELD, PARAMETER, LOCAL_VARIABLE, ANNOTATION_TYPE})
+@Language(value = "Arend", prefix = "\\func f => ")
+public @interface ArendExpr {
+}
