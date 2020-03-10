f5c26e95ecd45f1ea28495c4d991055e25356a45
ice1000 <ice1000kotlin@foxmail.com>
2020-03-10 18:21:08 -0400

    Test the error message

--- src/main/java/org/arend/typechecking/subexpr/SubExprError.java
+++ src/main/java/org/arend/typechecking/subexpr/SubExprError.java
@@ -1,8 +1,11 @@
 package org.arend.typechecking.subexpr;
 
 import org.arend.core.expr.Expression;
+import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Optional;
 
 public class SubExprError {
   private final @NotNull Kind kind;
@@ -33,47 +36,52 @@ public class SubExprError {
     return kind;
   }
 
+  @Override
+  public @NotNull @Nls String toString() {
+    switch (getKind()) {
+      case FieldNotFound:
+        return "a corresponding field is missing";
+      case MissingImplementationField:
+        return "the implementation field seems missing";
+      case MissingExpr:
+        return "an expression is missing, like in a clause";
+      case ExpectLam:
+        return "expect the lambda body " + errorExpr +
+            " to be another lambda expression";
+      case Clauses:
+        return "when matching the clauses of a let expression, the list doesn't match";
+      case Telescope:
+        return "when matching the parameter types of " +
+            Optional.ofNullable(errorExpr).map(Expression::toString).orElse("a telescopic expression") +
+            ", the list doesn't match";
+      case Arguments:
+        return "when matching the arguments of an application " + errorExpr +
+            ", the list doesn't match";
+      case ExpectPi:
+        return "expect the type " + errorExpr + " to be a pi type";
+      case ConcreteHasNoTypeBinding:
+        return "there isn't a type annotation in the binding of type " + errorExpr;
+      case ExprListNoMatch:
+        return "when matching a list of expressions, the list doesn't match";
+      case ConcreteCoreMismatch:
+        return "the core expression doesn't have the corresponded type of the concrete one";
+    }
+    return "unknown error happened";
+  }
+
+  /**
+   * @see SubExprError#toString()
+   */
   public enum Kind {
-    /**
-     * A corresponded field is missing.
-     */
     FieldNotFound,
-    /**
-     * An expression is missing, like in a clause.
-     */
     MissingImplementationField,
-    /**
-     * An expression is missing, like in a clause.
-     */
     MissingExpr,
-    /**
-     * Expect the lambda body to be another lambda expression, but it's not.
-     */
     ExpectLam,
-    /**
-     * When matching the clauses of a let expression, the list doesn't match.
-     */
     Clauses,
-    /**
-     * When matching the parameter types of a pi (or a sigma) expression,
-     * the list doesn't match.
-     */
     Telescope,
-    /**
-     * When matching the arguments of an application, the list doesn't match.
-     */
     Arguments,
-    /**
-     * Expect the expression's type to be a pi expression, but it's not.
-     */
     ExpectPi,
-    /**
-     * In an expression with binding, there isn't a type in the binding.
-     */
     ConcreteHasNoTypeBinding,
-    /**
-     * When matching a list of expressions, the list doesn't match.
-     */
     ExprListNoMatch,
     /**
      * Concrete expression and core expression doesn't have the corresponded type,
