ac0520899f266bd256863fddc1f5c06eb7e6d220
ice1000 <ice1000kotlin@foxmail.com>
2020-04-10 13:11:58 -0400

    Specify OutputFile for BuildPreludeTask

--- buildSrc/src/main/groovy/org/arend/gradle/BuildPreludeTask.groovy
+++ buildSrc/src/main/groovy/org/arend/gradle/BuildPreludeTask.groovy
@@ -1,7 +1,9 @@
 package org.arend.gradle
 
 import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.JavaExec
+import org.gradle.api.tasks.OutputFile
 
 class BuildPreludeTask extends JavaExec {
     {
@@ -11,5 +13,11 @@ class BuildPreludeTask extends JavaExec {
     }
 
     @Input
-    String projectVersion = project.version
+    private final String projectVersion = project.version
+
+    @InputFile
+    private final File preludeDotArd = project.rootProject.file("lib/Prelude.ard")
+
+    @OutputFile
+    private final File preludeDotArc = project.rootProject.file("lib/Prelude.arc")
 }
