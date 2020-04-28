266d92125fc5e0255036afcbe94437fc36dbe478
ice1000 <ice1000kotlin@foxmail.com>
2020-04-28 09:56:45 +0800

    Minor lexer refactoring (#135)

--- cli/src/main/java/org/arend/frontend/repl/ReplUtils.java
+++ cli/src/main/java/org/arend/frontend/repl/ReplUtils.java
@@ -11,23 +11,26 @@ import org.jetbrains.annotations.NotNull;
 
 public interface ReplUtils {
   static @NotNull ArendParser createParser(@NotNull String text, @NotNull ModulePath modulePath, @NotNull ErrorReporter reporter) {
-    CharStream input = CharStreams.fromString(text);
-    ArendLexer lexer = new ArendLexer(input);
-    lexer.removeErrorListeners();
     BaseErrorListener errorListener = new BaseErrorListener() {
       @Override
       public void syntaxError(Recognizer<?, ?> recognizer, Object o, int line, int pos, String msg, RecognitionException e) {
         reporter.report(new ParserError(new Position(modulePath, line, pos), msg));
       }
     };
-    lexer.addErrorListener(errorListener);
-
-    CommonTokenStream tokens = new CommonTokenStream(lexer);
-    ArendParser parser = new ArendParser(tokens);
+    ArendParser parser = new ArendParser(
+        new CommonTokenStream(createLexer(text, errorListener)));
     parser.removeErrorListeners();
     parser.addErrorListener(errorListener);
     // parser.addErrorListener(new DiagnosticErrorListener());
     // parser.getInterpreter().setPredictionMode(PredictionMode.LL_EXACT_AMBIG_DETECTION);
     return parser;
+  }
+
+  static @NotNull ArendLexer createLexer(@NotNull String text, BaseErrorListener errorListener) {
+    CharStream input = CharStreams.fromString(text);
+    ArendLexer lexer = new ArendLexer(input);
+    lexer.removeErrorListeners();
+    lexer.addErrorListener(errorListener);
+    return lexer;
   }
 }
