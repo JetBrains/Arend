1b03f026e8fd28ed6c4bdb20df4b306dbda42119
ice1000 <ice1000kotlin@foxmail.com>
2020-05-11 07:24:42 +0800

    Comments

--- base/src/main/java/org/arend/repl/CommandHandler.java
+++ base/src/main/java/org/arend/repl/CommandHandler.java
@@ -48,7 +48,7 @@ public final class CommandHandler implements ReplHandler {
         .mapToInt(String::length)
           .summaryStatistics();
       int maxWidth = Math.min(statistics.getMax(), 8) + 1;
-      api.println("There are " + statistics.getCount() + " action(s) available.");
+      api.println("There are " + statistics.getCount() + " actions available.");
       for (var replCommand : commandMap.entrySet()) {
         var description = replCommand.getValue().description();
         if (description == null) continue;
--- base/src/main/java/org/arend/repl/QuitReplException.java
+++ base/src/main/java/org/arend/repl/QuitReplException.java
@@ -1,5 +1,8 @@
 package org.arend.repl;
 
+/**
+ * To indicate a request to quit the REPL.
+ */
 public class QuitReplException extends Exception {
   public QuitReplException() {
   }
