8ac74fc5ab2f40735dc2220f2f4e75cfaf9e5be5
ice1000 <ice1000kotlin@foxmail.com>
2020-07-30 01:32:59 +0800

    Test definable meta parameter resolving (#233)

--- src/test/java/org/arend/naming/DefinableMetaTest.java
+++ src/test/java/org/arend/naming/DefinableMetaTest.java
@@ -5,14 +5,18 @@ import org.junit.Test;
 public class DefinableMetaTest extends NameResolverTestCase {
   @Test
   public void noArgDef() {
-    var m = resolveNamesModule("\\meta sepush => 123");
+    resolveNamesModule("\\meta sepush => 123");
+  }
+
+  @Test
+  public void someArgsDef() {
+    var m = resolveNamesModule("\\meta sepush a b c => a b c");
     System.out.println(m);
   }
 
   @Test
   public void noArgCall() {
-    var m = resolveNamesModule("\\meta sepush => 123" +
+    resolveNamesModule("\\meta sepush => 123" +
       "\\func qlbf => sepush");
-    System.out.println(m);
   }
 }
