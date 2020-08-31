5d9d0ddae58d0818d307dd3b7c09e95808919d7d
valis <valery.isaev@gmail.com>
2020-08-31 16:34:27 +0300

    Remove unused code

--- base/src/main/java/org/arend/typechecking/provider/ConcreteProvider.java
+++ base/src/main/java/org/arend/typechecking/provider/ConcreteProvider.java
@@ -10,12 +10,4 @@ public interface ConcreteProvider {
   @Nullable Concrete.FunctionDefinition getConcreteInstance(GlobalReferable referable);
   @Nullable Concrete.ClassDefinition getConcreteClass(GlobalReferable referable);
   @Nullable Concrete.DataDefinition getConcreteData(GlobalReferable referable);
-
-  @Nullable default TCReferable getTCReferable(GlobalReferable referable) {
-    if (referable instanceof TCReferable) {
-      return (TCReferable) referable;
-    }
-    var def = getConcrete(referable);
-    return def instanceof Concrete.ReferableDefinition ? ((Concrete.ReferableDefinition) def).getData() : null;
-  }
 }
