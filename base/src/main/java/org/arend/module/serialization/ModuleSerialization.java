92ad77e40f12bdd2aa40453d4e115a19a61ca721
valis <valery.isaev@gmail.com>
2020-10-09 20:19:08 +0300

    1.4.1 -> 1.5

--- base/src/main/java/org/arend/module/serialization/ModuleSerialization.java
+++ base/src/main/java/org/arend/module/serialization/ModuleSerialization.java
@@ -21,7 +21,7 @@ public class ModuleSerialization {
   private final Set<Integer> myCurrentDefinitions = new HashSet<>();
   private boolean myComplete;
 
-  static final int VERSION = 5;
+  static final int VERSION = 6;
 
   public ModuleSerialization(ErrorReporter errorReporter, DependencyListener dependencyListener) {
     myErrorReporter = errorReporter;
