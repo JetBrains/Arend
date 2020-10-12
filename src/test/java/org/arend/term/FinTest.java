413c1e650c06eb0d8e88fa30f5868038f8cca43c
ice1000 <ice1000kotlin@foxmail.com>
2020-10-13 05:25:28 +0800

    Add passing test

---
+++ src/test/java/org/arend/term/FinTest.java
@@ -0,0 +1,25 @@
+package org.arend.term;
+
+import org.arend.typechecking.TypeCheckingTestCase;
+import org.junit.Test;
+
+public class FinTest extends TypeCheckingTestCase {
+  @Test
+  public void emptySet() {
+    typeCheckDef("\\func xy (_ : Fin 0) : Empty" +
+      "\\where \\data Empty");
+  }
+
+  @Test
+  public void literalFin() {
+    typeCheckDef("\\func ren : Fin 1 => 0");
+    typeCheckDef("\\func xyr : Fin 2 => 1");
+    typeCheckDef("\\func xyren : Fin 101 => 100");
+  }
+
+  @Test
+  public void unfiniteFin() {
+    typeCheckDef("\\func dalao : 0 = {Nat} (\\let | x : Fin 1 => 0 \\in x) => idp");
+    typeCheckDef("\\func julao : 0 = {Fin 1} (\\let | x : Fin 1 => 0 \\in x) => idp");
+  }
+}
