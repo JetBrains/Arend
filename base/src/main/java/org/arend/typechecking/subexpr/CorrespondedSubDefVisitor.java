45a177d0473690e6dd26de0329f75b8d5b3e40fd
valis <valery.isaev@gmail.com>
2021-03-05 23:29:48 +0300

    Fix NPE

--- base/src/main/java/org/arend/typechecking/subexpr/CorrespondedSubDefVisitor.java
+++ base/src/main/java/org/arend/typechecking/subexpr/CorrespondedSubDefVisitor.java
@@ -132,7 +132,7 @@ public class CorrespondedSubDefVisitor implements
             .map(coreDef::getImplementation);
         if (field.isEmpty()) continue;
         // The binding is `this` I believe
-        var accept = concrete.implementation.accept(visitor, field.get().getExpression());
+        var accept = concrete.implementation == null ? null : concrete.implementation.accept(visitor, field.get().getExpression());
         if (accept != null) return accept;
       }
     return null;
