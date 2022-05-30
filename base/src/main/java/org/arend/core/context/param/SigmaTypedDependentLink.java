c548ee6954942478fcb01a17b96bd0d0b2c2a07d
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2022-05-30 09:14:02 +0300

    Add .editorconfig to the Arend project

--- base/src/main/java/org/arend/core/context/param/SigmaTypedDependentLink.java
+++ base/src/main/java/org/arend/core/context/param/SigmaTypedDependentLink.java
@@ -6,30 +6,30 @@ import org.arend.core.subst.SubstVisitor;
 import org.arend.ext.concrete.expr.SigmaFieldKind;
 
 public class SigmaTypedDependentLink extends TypedDependentLink {
-    private final SigmaFieldKind explicitKindMark;
+  private final SigmaFieldKind explicitKindMark;
 
-    public SigmaTypedDependentLink(String name, Type type, DependentLink next, SigmaFieldKind explicitKindMark) {
-        super(true, name, type, false, next);
-        this.explicitKindMark = explicitKindMark;
-    }
+  public SigmaTypedDependentLink(String name, Type type, DependentLink next, SigmaFieldKind explicitKindMark) {
+    super(true, name, type, false, next);
+    this.explicitKindMark = explicitKindMark;
+  }
 
-    public SigmaFieldKind getFieldKind() {
-        return explicitKindMark;
-    }
+  public SigmaFieldKind getFieldKind() {
+    return explicitKindMark;
+  }
 
-    @Override
-    public DependentLink subst(SubstVisitor substVisitor, int size, boolean updateSubst) {
-        if (size > 0) {
-            TypedDependentLink result = new SigmaTypedDependentLink( getName(), getType().subst(substVisitor), EmptyDependentLink.getInstance(), explicitKindMark);
-            if (updateSubst) {
-                substVisitor.getExprSubstitution().addSubst(this, new ReferenceExpression(result));
-            } else {
-                substVisitor.getExprSubstitution().add(this, new ReferenceExpression(result));
-            }
-            result.setNext(getNext().subst(substVisitor, size - 1, updateSubst));
-            return result;
-        } else {
-            return EmptyDependentLink.getInstance();
-        }
+  @Override
+  public DependentLink subst(SubstVisitor substVisitor, int size, boolean updateSubst) {
+    if (size > 0) {
+      TypedDependentLink result = new SigmaTypedDependentLink(getName(), getType().subst(substVisitor), EmptyDependentLink.getInstance(), explicitKindMark);
+      if (updateSubst) {
+        substVisitor.getExprSubstitution().addSubst(this, new ReferenceExpression(result));
+      } else {
+        substVisitor.getExprSubstitution().add(this, new ReferenceExpression(result));
+      }
+      result.setNext(getNext().subst(substVisitor, size - 1, updateSubst));
+      return result;
+    } else {
+      return EmptyDependentLink.getInstance();
     }
+  }
 }
